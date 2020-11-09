dac570471f1254de78854edeb0b235ef7cd42af7
Alfred Adeoye <alfred.adeoye@cantab.net>
2020-11-09 15:10:29 +0000

    v0.8b fix vscode issue

--- components/SkillForm.jsx
+++ components/SkillForm.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Input from "./Input"
 import SubmitButton from "./SubmitButton"
 import styles from "../styles/components/form.module.scss"
@@ -46,53 +45,4 @@ const SkillForm = (props) => {
       )
 }
 
-=======
-import Input from "./Input"
-import SubmitButton from "./SubmitButton"
-import styles from "../styles/components/form.module.scss"
-import { useState } from "react"
-
-const SkillForm = (props) => {
-      const [ input, setInput ] = useState("")
-
-      const handleChange = (e) => {
-            setInput(e.target.value)
-      }
-
-      const handleSubmit = (e) => {
-            e.preventDefault()
-            if(input !== "") {
-                  fetch("/skill", {
-                        method: "POST",
-                        headers: {
-                              "content-type": "application/json"
-                        },
-                        body: JSON.stringify({ skill: input })
-                  })
-                  .then(res => res.json())
-                  .then(data => { 
-                        console.log(data.message)
-                        props.close()
-                        setInput("")
-                  })
-                  .catch(err => console.error(err))
-            } 
-      }
-
-      return (
-            <form className={styles.form} onSubmit={handleSubmit}>
-                  <Input 
-                  type="text"
-                  name="skill"
-                  placeholder="Enter Skill"
-                  autoComplete="off"
-                  handleChange={handleChange}
-                  value={input}
-                  />
-                  <SubmitButton />
-            </form>
-      )
-}
-
->>>>>>> ae3b55c0fda5c438f46c6b4cebd6008143645b37
 export default SkillForm
--- layouts/Dashboard.jsx
+++ layouts/Dashboard.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { useState } from "react"
 import SkillForm from "../components/SkillForm"
 import ProjectForm from "../components/ProjectForm"
@@ -32,39 +31,4 @@ const DashboardLayout = () => {
       )
 }
 
-=======
-import { useState } from "react"
-import SkillForm from "../components/SkillForm"
-import ProjectForm from "../components/ProjectForm"
-import Modal from "../components/Modal"
-import CloseButton from "../components/CloseButton"
-import styles from "../styles/layouts/dashboard.module.scss"
-
-const DashboardLayout = () => {
-      const [ views, setViews ] = useState("menu")
-
-      const handleClick = () => setViews("menu")
-
-      return (
-            <section className={styles.dashboard}>
-                  {views === "menu" ? 
-                  <span>
-                  <button onClick={() => setViews("skills")}><p>Add New Skill</p></button>
-                  <button onClick={() => setViews("projects")}><p>Add New Project</p></button>
-                  </span> :
-                  views === "skills" ? 
-                  <Modal>
-                        <CloseButton handleClick={handleClick}/>
-                        <SkillForm close={handleClick}/>
-                  </Modal> :
-                  views === "projects" ?
-                  <Modal>
-                        <CloseButton handleClick={handleClick}/>
-                        <ProjectForm close={handleClick}/>
-                  </Modal> : false }
-            </section>
-      )
-}
-
->>>>>>> ae3b55c0fda5c438f46c6b4cebd6008143645b37
 export default DashboardLayout
